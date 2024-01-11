6db95f53ee58cf3859c50d0ea4137e439b5b2e5f
Curtis CHROME <mrmr450@gmail.com>
2024-01-11 13:36:42 +0900

    Implement game logic and counters
    Highlights are toggling on and off. Winner is being declared,
    win count is calculated and text is being displayed.

--- js.js
+++ js.js
@@ -1,4 +1,8 @@
-
+let gameIsDraw;
+let playerWinCount = 0;
+let computerWinCount = 0;
+let clickedImage;
+let shoutout = document.querySelector('.shoutout');
 
 // GET computer choice of rock, scissor, or paper
 function getComputerChoice() {
@@ -7,44 +11,89 @@ function getComputerChoice() {
     return (computerChoice[random]);
 }
 
-// GET player selection of rock, paper, or paper
-// Validate this case insensitive
-function getPlayerChoice() {
-    let choice;
-    do {
-        choice = prompt("Please choose rock, paper, or scissors.").toLowerCase();
-        if (choice === null) {
-            break;
-        }
-    } while (choice !== "rock" && choice !== "paper" && choice !== "scissors");
-    return choice;
+// GET player's choice from images
+const buttons = document.querySelector('#player-images');
+buttons.addEventListener('click', function(e) {
+    if (e.target.tagName === 'IMG') {
+        clickedImage = e.target;
+        let playerChoiceID = clickedImage.id;
+
+        clickedImage.classList.toggle("active-image");
+
+        playRound(getComputerChoice(), playerChoiceID);
+    }
+})
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
 }
 
-function playRound(computer, player) {
+function updateComputerImg(computer) {
+    let computerIMG = document.querySelector('#question');
+    computerIMG.src = "img/" + computer + ".png";
+    computerIMG.alt = computer;
+    computer.width = "110px";
+    computer.height = "110px";
+}
+
+function resetElements() {
+    shoutout.textContent = "Choose Rock, Paper, or Scissors";
+    clickedImage.classList.toggle("active-image");
+}
+
+function updateWinnerText(winner, message) {
+    let winnerDeclaration = document.querySelector('.who-wins');
+    let winnerDetails = document.querySelector('.how-win');
+
+    winnerDeclaration.textContent = winner;
+    winnerDetails.textContent = message;
+}
+
+async function playRound(computer, player) {
     let winner;
+
+    updateComputerImg("question");
+    updateWinnerText("", "");
+
+    shoutout.textContent = "さいしょ　は　ぐー";
+    await delay(1800);
+    shoutout.textContent = "じゃんけん";
+    await delay(800);
+    shoutout.textContent = "ぽい！";
+
+    updateComputerImg(computer);
+
     if (player === computer) {
-        alert(`Draw.
-                Player: ${player}... Games won: ${playerWin} 
-                Computer: ${computer}... Games won: ${computerWin}
-                Play again!`);
-        gameIsDraw = true;
         winner = "draw";
+        updateWinnerText("Draw!", "Try again!");
+
+        gameIsDraw = true;
+        
+        resetElements();
     } else if ((player === "rock" && computer === "scissors") ||
                 (player === "paper" && computer === "rock") ||
                 (player === "scissors" && computer === "paper")) {
-        playerWin += 1;
-        alert(`You win!
-                Player: ${player}... Games won: ${playerWin} 
-                Computer: ${computer}... Games won: ${computerWin}`);
-        gameIsDraw = false;
         winner = "player";
-    } else {
-        computerWin += 1;
-        alert(`You lose!
-                Player: ${player}... Games won: ${playerWin} 
-                Computer: ${computer}... Games won: ${computerWin}`);
+        updateWinnerText("Player wins!", `${player} beats ${computer}`);
+        
+        let playerWinCountDisplay = document.querySelector("#player-win-counter");
+        playerWinCount += 1;
+        playerWinCountDisplay.textContent = playerWinCount;
+
         gameIsDraw = false;
+        
+        resetElements();
+    } else {
         winner = "computer";
+        updateWinnerText("Computer wins!", `${computer} beats ${player}`);
+        
+        let computerWinCountDisplay = document.querySelector("#computer-win-counter");
+        computerWinCount += 1;
+        computerWinCountDisplay.textContent = computerWinCount;
+
+        gameIsDraw = false;
+        
+        resetElements();
     }
     return winner;
 }
@@ -57,19 +106,10 @@ function game() {
             let computerSelection = getComputerChoice();
             let playerSelection = getPlayerChoice();
 
-            console.log(computerSelection);
-            console.log(playerSelection);
-
             gameWinner = playRound(computerSelection, playerSelection);
         } while (gameIsDraw);
     }
-    alert(`Best-of-five results:
-            Player: ${playerWin} games won 
-            Computer: ${computerWin} games won`);
-}
-
-let gameIsDraw;
-let playerWin = 0;
-let computerWin = 0;
-
-game();
+//     alert(`Best-of-five results:
+//             Player: ${playerWin} games won 
+//             Computer: ${computerWin} games won`);
+// }
